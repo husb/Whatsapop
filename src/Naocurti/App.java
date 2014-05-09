2215af679862c6b3e64e4947d44f1cfd7092565a
husb <husbeh@gmail.com>
2014-05-09 10:41:06 -0300

    .

--- src/Naocurti/App.java
+++ src/Naocurti/App.java
@@ -1,6 +1,13 @@
 package Naocurti;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
 import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 /**
  * 
@@ -12,7 +19,7 @@ public class App {
 
 	public static void main(String[] args) throws SQLException {
 
-		UsuarioDao e = new UsuarioDao();
+	/*	UsuarioDao e = new UsuarioDao();
 
 		e.conectar();
 
@@ -26,8 +33,33 @@ public class App {
 		} catch (Exception ex) {
 			System.out.println("Informação não encontrada.");
 
-		}
+		}  */
+		
+		MulticastSocket socket = null;
 
-	}
+        try{
+            InetAddress grupo = InetAddress.getByName("228.5.6.7");
+            socket = new MulticastSocket(6789);
+            socket.joinGroup(grupo);
+            byte[] msg = JOptionPane.showInputDialog("Digite a mensagem: ").getBytes();
+            DatagramPacket mensagemOut = new DatagramPacket(msg, msg.length,grupo, 6789);
+            socket.send(mensagemOut);
+
+            for(int i=0;i<10;i++)
+            {
+                byte[] buffer = new byte[1000];
+                DatagramPacket mensagemIn = new DatagramPacket(buffer, buffer.length);
+                
+                socket.receive(mensagemIn);
+                System.out.println("Recebido de "+InetAddress.getByAddress(mensagemIn.getAddress().getAddress())
+                        +" a mensagem: "+new String(mensagemIn.getData()));
+            }
+            socket.leaveGroup(grupo);
+        }catch(SocketException se){System.out.println("Problema no Socket: "+se.getMessage());
+        }catch(IOException ie){System.out.println("Problema no I/O: "+ie.getMessage());
+        }finally{if(socket!=null) socket.close();
+        
+		}
+	}  
 
 }
